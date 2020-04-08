0b809b49cb077896e698831fcff09a4dab5e237a
Joe Zugelter <zugelter@gmail.com>
2020-04-08 13:23:40 -0400

    vuetify theme changed

--- vue_starter_dotnet/frontend/src/plugins/vuetify.js
+++ vue_starter_dotnet/frontend/src/plugins/vuetify.js
@@ -1,7 +1,26 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+    theme: {
+        themes: {
+            dark: {
+        primary: '#264653',
+        secondary: '#2A9D8F',
+        accent: '#E9C46A',
+        error: '#FF5252',//not our choice
+        info: '#F4A261',
+        success: '#E76F51',
+        warning: '#FFC107',//not our choice
+    }}}
+});
+
+
+
+// Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        dark: true
+    }
 });
