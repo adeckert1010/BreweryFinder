749307561e4d4efbeab99c3f7efec2e370cd0fad
adeckert1010 <adeckert1010@gmail.com>
2020-04-10 13:49:48 -0400

    worked on the brewery cards more

--- vue_starter_dotnet/frontend/src/components/Brewery.vue
+++ vue_starter_dotnet/frontend/src/components/Brewery.vue
@@ -6,25 +6,43 @@
         :src="brewery.imageLocation"
         :alt="brewery.name"
         @click="isHidden = !isHidden"
-        class="white--text"
+        class="align-end"
       ></v-img>
-      <v-card-title>{{brewery.name}}</v-card-title>
-      <v-card-actions>
-        <v-btn icon @click="isHidden = !isHidden">
-          <v-icon>{{isHidden ? 'mdi-chevron-up' : 'mdi-chevron-down'}}</v-icon>
-        </v-btn>
-      </v-card-actions>
+      <v-card-title class="headline text--secondary">
+        <strong>{{brewery.name}}</strong>
+        <v-spacer></v-spacer>
+        <v-card-actions>
+          <v-btn icon @click="isHidden = !isHidden">
+            <v-icon>{{isHidden ? 'mdi-chevron-up' : 'mdi-chevron-down'}}</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card-title>
       <v-expand-transition>
-        <div v-show="isHidden">
+        <v-card-text v-show="isHidden">
           <v-divider></v-divider>
-          <p>{{brewery.address_1}} {{brewery.address_2}} {{brewery.city}}, {{brewery.district}} {{brewery.zipCode}}</p>
-          <p>{{brewery.type}}</p>
-          <p v-if="brewery.established">{{brewery.established}}</p>
-          <p>{{brewery.backgroundInfo}}</p>
-          <v-card class="accent" v-for="beer in beers" v-bind:key="beer.id">
-            <beer v-bind:beer="beer"></beer>
-          </v-card>
-        </div>
+          {{brewery.address_1}} {{brewery.address_2}} {{brewery.city}}, {{brewery.district}} {{brewery.zipCode}}
+          <br />
+          {{brewery.type}}
+          <p v-if="brewery.established">Established: {{brewery.established}}</p>
+          <br />
+          {{brewery.backgroundInfo}}
+          <v-card-actions class="justify-center">
+            <v-btn text @click="isHiddenBeer = !isHiddenBeer">View Beers</v-btn>
+          </v-card-actions>
+          <v-expand-transition>
+            <v-container v-show="isHiddenBeer">
+              <v-card
+                class="accent mx-auto"
+                v-for="beer in beers"
+                v-bind:key="beer.id"
+                max-width="95%"
+                hover
+              >
+                <beer v-bind:beer="beer"></beer>
+              </v-card>
+            </v-container>
+          </v-expand-transition>
+        </v-card-text>
       </v-expand-transition>
       <!-- <router-link>See Beers available</router-link> -->
     </v-card>
@@ -38,7 +56,8 @@ export default {
   data() {
     return {
       isHidden: false,
-      beers: []
+      beers: [],
+      isHiddenBeer: false
     };
   },
   components: {
