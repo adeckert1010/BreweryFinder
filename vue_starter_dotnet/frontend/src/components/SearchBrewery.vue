4c9d94588cdfc0464e2c6440875292b28bbdaa45
Travis Hyde <travis.e.hyde@gmail.com>
2020-04-10 16:06:00 -0400

    brewery search functionality...it is there

---
+++ vue_starter_dotnet/frontend/src/components/SearchBrewery.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="text" id="search" name="search" v-on:keyup="filter" placeholder="Search Breweries" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      apiURL: ""
+      
+    };
+  },
+  methods: {
+    filter() {
+      const query = document.getElementById("search").value;
+      this.$emit("filter", query);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
