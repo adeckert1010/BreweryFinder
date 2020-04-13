b948ae380836a891922bc2ef6797116dfdadfce5
adeckert1010 <adeckert1010@gmail.com>
2020-04-13 15:42:14 -0400

    a couple more changes to nav

--- vue_starter_dotnet/frontend/src/App.vue
+++ vue_starter_dotnet/frontend/src/App.vue
@@ -5,22 +5,22 @@
         <router-link id="nav-bar-title" :to="{name: 'home'}"> <v-icon class="pa-2 ma-2" color="info">mdi-glass-mug-variant</v-icon>Brewery Friend Finder</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn class="pa-1 ma-1" v-if="getUser()">{{getUser().sub}}</v-btn>
-      <v-btn class="pa-1 ma-1" to="/">
+      <v-btn class="pa-1 ma-1 info" v-if="getUser()">{{getUser().sub}}</v-btn>
+      <v-btn class="pa-1 ma-1 info" to="/">
         Home
         <!-- <router-link :to="{name: 'home'}">Home</router-link> -->
       </v-btn>
       <!-- <span v-if="$auth.loading"> -->
       <!-- <v-btn @authenticated="user=getUser"  v-if="user" to="/logout" > -->
-      <v-btn class="pa-1 ma-1" v-if="getUser()" to="/logout">
+      <v-btn class="pa-1 ma-1 accent" v-if="getUser()" to="/logout">
         Logout
         <!-- <router-link :to="{name: 'logout'}">Logout</router-link> -->
       </v-btn>
-      <v-btn class="pa-1 ma-1" v-if="!getUser()" to="/login">
+      <v-btn class="pa-1 ma-1 info" v-if="!getUser()" to="/login">
         Login
         <!-- <router-link :to="{name: 'login'}">Login</router-link> -->
       </v-btn>
-      <v-btn class="pa-1 ma-1" v-if="!getUser()" to="/register">
+      <v-btn class="pa-1 ma-1 info" v-if="!getUser()" to="/register">
         Register
         <!-- <router-link :to="{name: 'login'}">Login</router-link> -->
       </v-btn>
