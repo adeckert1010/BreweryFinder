5bccdd14b20732e3af04e5e577b07beda8e7fbbe
Travis Hyde <travis.e.hyde@gmail.com>
2020-04-13 16:02:01 -0400

    look and feel stuff on beer brewery and single beer views

--- vue_starter_dotnet/frontend/src/components/Beer.vue
+++ vue_starter_dotnet/frontend/src/components/Beer.vue
@@ -1,13 +1,14 @@
 <template>
-<v-container>
-  <v-content class="beer mx-auto pa-0">
-      <v-card-title>{{beer.name}}</v-card-title>
-      <p>{{beer.description}}</p>
-      <v-img :src="beer.imageLocation" :alt="beer.beer_name" class="beer_image justify-center" max-width="50%"></v-img>
-      <p>ABV: {{beer.abv}}</p>
+<v-container fluid>
+  <v-content >
+      <v-card-title class="ma-0 pa-0 font-weight-bold" >{{beer.name}}</v-card-title>
+      <p class=font-italic>{{beer.description}}</p>
+      <v-img :src="beer.imageLocation"  :alt="beer.beer_name" height="200px" width="100px" ></v-img>
+      <p>ABV: {{beer.abv}}%</p>
       <p>IBU: {{beer.ibu}}</p>
-      <p>{{beer.type}}</p>
-      <router-link :to="{name: 'singleBeer', params: {id: beer.id}}">View beer info</router-link>
+      <p>Type: {{beer.type}}</p>
+     
+      
   </v-content>
 </v-container>
 </template>
