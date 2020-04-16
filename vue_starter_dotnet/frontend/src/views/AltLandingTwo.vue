4060b2734b3ae34b1f075e9080adf2c98a6190c3
Joe Zugelter <zugelter@gmail.com>
2020-04-16 09:42:46 -0400

    alttwo search page now better responsive design

--- vue_starter_dotnet/frontend/src/views/AltLandingTwo.vue
+++ vue_starter_dotnet/frontend/src/views/AltLandingTwo.vue
@@ -1,52 +1,31 @@
 <template>
   <v-content class="altlanding secondary">
-    <v-row>
-        <v-col 
-            md=1
-            cols="12"
-            sm="4">
-    
-</v-col>
-        <v-col
-            md=4
-            cols="12"
-            sm="4">
-    <v-card dark class="accent">
-        <brewery-search-card></brewery-search-card>
-    </v-card>
-</v-col>
+    <v-row align="center">
+      <v-col cols="12" md="1"  sm="1"></v-col>
+      <v-col cols="12"  md="4" sm="10">
+        <v-card dark class="accent">
+          <brewery-search-card></brewery-search-card>
+        </v-card>
+      </v-col>
 
-<v-col 
-            md=2
-            cols="12"
-            sm="4">
-    <v-card dark class="accent">
-        
-    </v-card>
-</v-col>
+      <v-col  cols="12" md="1" sm="1">
+        <v-card dark class="accent"></v-card>
+      </v-col>
+      <v-col  cols="12" md="1" sm="1">
+        <v-card dark class="accent"></v-card>
+      </v-col>
 
- <v-col 
-            md=4
-            cols="12"
-            sm="4">
-    <v-card dark class="accent">
-        <search-beers></search-beers>
-    </v-card>
-    <v-col 
-            md=1
-            cols="12"
-            sm="4">
-    
-</v-col>
-</v-col>
-
- 
-</v-row>
+      <v-col  cols="12" md="4" sm="10">
+        <v-card dark class="accent">
+          <search-beers></search-beers>
+        </v-card>
+        <v-col  cols="12" md="1" sm="1"></v-col>
+      </v-col>
+    </v-row>
   </v-content>
 </template>
 
 <script>
-
 import SearchBeers from "@/components/SearchBeers.vue";
 import BrewerySearchCard from "@/components/BrewerySearchCard.vue";
 
@@ -56,16 +35,16 @@ export default {
     return {
       breweries: [],
       images: []
-    }
+    };
   },
   components: {
     SearchBeers,
     BrewerySearchCard
   },
   methods: {
-    setImages(){
+    setImages() {
       this.breweries.forEach(brewery => {
-        this.images.push(brewery.imageLocation)
+        this.images.push(brewery.imageLocation);
       });
     }
   },
